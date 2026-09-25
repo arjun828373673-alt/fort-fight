11c46ddd10d6658ef4b169bc8c19cb31fb56314d
Arjun Mishra <arjun828373673@gmail.com>
2026-09-25 09:44:24 +0530

    Auto-commit: script.js at 9/25/2026, 9:44:24 AM

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 document.getElementById("start-button").addEventListener("click",() => showView("gameCanvas")); 
+
+const gameCanvas = document.getElementById("gameCanvas");
+const ctx = gameCanvas.getContext("2d");
+ gameCanvas.width = window.innerWidth;
+ gameCanvas.height = window.innerHeight;
+
+ const player = {
+  x: 50,
+  y: 300,
+  width: 50,
+  height: 50,
+  color: "red"
+ };
+ ctx.fillStyle = player.color;
+ ctx.fillRect(player.x, player.y, player.width, player.height);
