8d9103ed1b619cbf3823b7b5a037200734b956a4
Arjun Mishra <arjun828373673@gmail.com>
2026-09-25 09:17:44 +0530

    Auto-commit: script.js at 9/25/2026, 9:17:44 AM

---
+++ script.js
@@ -0,0 +1,5 @@
+Document.getElementById("gameCanvas").addEventListener("click", function() {
+    // Get the canvas element and its context
+    var canvas = document.getElementById("gameCanvas");
+    var ctx = canvas.getContext("2d");
+});
